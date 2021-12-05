29c15d50b3aa0a22b8b6e64a0804d6061fa20c1f
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-12-05 18:45:53 +0530

    Added peerConnection.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,28 +1,25 @@
-import firebase from "firebase/compat/app";
-import database from "firebase/compat/database";
+import firebase from "firebase";
 
-const firebaseCofig = {
+var firebaseConfig = {
   apiKey: "AIzaSyD_2GppEUFcKVDODmDDO9j1ktWhJ4CeibA",
   databaseURL:
     "https://meet-clone-460b8-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
+firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseCofig);
+export const db = firebase;
 
-let dbRef = firebase.database().ref();
+var firepadRef = firebase.database().ref();
 
-export const userName = prompt("Whats your Name?");
-
-const urlPramas = new URLSearchParams(window.location.search);
-
-const roomId = urlPramas.get("id");
+export const userName = prompt("What's your name?");
+const urlparams = new URLSearchParams(window.location.search);
+const roomId = urlparams.get("id");
 
 if (roomId) {
-  dbRef = dbRef.child(roomId);
+  firepadRef = firepadRef.child(roomId);
 } else {
-  dbRef = dbRef.push();
+  firepadRef = firepadRef.push();
+  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
 }
 
-window.history.replaceState(null, "Meet", "?id=" + dbRef.key);
-
-export default dbRef;
+export default firepadRef;
